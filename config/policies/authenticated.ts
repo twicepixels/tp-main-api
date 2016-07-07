26107927f45954c204dbc7ae3d5cf741410f377b
dfonseca <tuma05@gmail.com>
2016-07-06 23:15:40 -0600

    Habilitar CORS mediante Express

--- config/policies/authenticated.ts
+++ config/policies/authenticated.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 
 module.exports = function (req: Request, res: Response, next: any) {
-	// User is allowed, proceed to controller
-	var is_auth = req.isAuthenticated();
-	if (is_auth) return next();
-	// User is not allowed
-	return res.send(401);
+  // User is allowed, proceed to controller
+  var is_auth = req.isAuthenticated();
+  if (is_auth) return next();
+  // User is not allowed
+  return res.sendStatus(401);
 };
--- config/routes/app.router.ts
+++ config/routes/app.router.ts
@@ -1,22 +1,33 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 var authenticatedPolicy = require("../policies/authenticated");
 import { AUTH_ROUTER } from "./auth.route";
 import { USER_ROUTER } from "./user.route";
 
 export class AppRouter {
-	public static config(router: Router): void {
-		// middleware that is specific to this router
-		router.use(function timeLog(req, res, next) {
-			console.log('Time: ', Date.now());
-			next();
-		});
-		var APP_ROUTER: Router = Router();
-		// define routes
-		APP_ROUTER.get("/", function (req, res) {
-			res.send('Twicepixels on-line');
-		});
-		router.use("/", APP_ROUTER);
-		router.use("/auth", AUTH_ROUTER);
-		router.use("/users", authenticatedPolicy, USER_ROUTER);
-	}
+  public static config(router: Router): void {
+    // middleware that is specific to this router -- CORS
+    router.use(function (req: Request, res: Response, next: any) {
+      console.log('Time: ', Date.now());
+      // res.header("Content-Type", "application/json");
+      res.setHeader('Access-Control-Allow-Credentials', "true");
+      res.header("Access-Control-Allow-Origin", req.header("Origin"));
+      res.header("Access-Control-Allow-Methods", "GET,POST,PUT,HEAD,DELETE,OPTIONS");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+      }
+      else {
+        next();
+      }
+    });
+
+    var APP_ROUTER: Router = Router();
+    // define routes
+    APP_ROUTER.get("/", function (req, res) {
+      res.send('Twicepixels on-line');
+    });
+    router.use("/", APP_ROUTER);
+    router.use("/auth", AUTH_ROUTER);
+    router.use("/users", authenticatedPolicy, USER_ROUTER);
+  }
 }
