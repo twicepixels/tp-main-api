f002f0f9a1cad31429c3dceb363fb7caa3b01b3f
Gabriel <sanabriaGabriel@gmail.com>
2016-07-23 21:55:39 -0600

    Subir metodos get y delete de usuarios

--- app/controllers/user.controller.ts
+++ app/controllers/user.controller.ts
@@ -13,4 +13,28 @@ export class UserController {
 			}
 		});
 	}
+
+  public static get(req: Request, res: Response) {
+    UserService.get(null, function (err: any, result: any) {
+      if (err) {
+        res.status(500).send({
+          message: err.message
+        });
+      } else {
+        res.send(result);
+      }
+    });
+  }
+
+  public static delete(req: Request, res: Response) {
+    UserService.delete(null, function (err: any, result: any) {
+      if (err) {
+        res.status(500).send({
+          message: err.message
+        });
+      } else {
+        res.send(result);
+      }
+    });
+  }
 }
--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -12,4 +12,28 @@ export class UserService extends Service {
 			next(error, null);
 		});
 	}
+
+  public static get(criteria: any, next: any){
+    this.Models.User.find({
+      where: criteria
+    }).then(function (user: any) {
+      // was found successfully!
+      next(null, user);
+    }, function (error: any) {
+      // error handling
+      next(error, null);
+    });
+  }
+
+  public static delete(criteria: any, next: any){
+    this.Models.User.delete({
+      where: criteria
+    }).then(function (user: any) {
+      // was found successfully!
+      next(null, user);
+    }, function (error: any) {
+      // error handling
+      next(error, null);
+    });
+  }
 }
