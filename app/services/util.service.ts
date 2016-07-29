379ca98e2755332a2230d8d1cbaea43707f10f21
dfonseca <tuma05@gmail.com>
2016-07-29 14:40:12 -0600

    Corregir nombre de variable

--- app/services/util.service.ts
+++ app/services/util.service.ts
@@ -2,14 +2,15 @@ import { Service } from "../../base/base.service";
 
 export class UtilService extends Service {
   public static getCatalog(criteria: any, next: any) {
-    this.Models.Catalog.findAll({
-      where: criteria
-    }).then(function (user: any) {
-      // was found successfully!
-      next(null, user);
-    }, function (error: any) {
-      // error handling
-      next(error, null);
-    });
+    this.Models.Catalog.findAll({where: criteria}).then(
+      (result: any)=> {
+        // was found successfully!
+        next(null, result);
+      },
+      (error: any)=> {
+        // error handling
+        next(error, null);
+      }
+    );
   }
 }
