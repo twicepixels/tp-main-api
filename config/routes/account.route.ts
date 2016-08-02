01b9241d4de9eb57a4d4c3ea7649336324e013ba
eduray <egcabc@gmail.com>
2016-08-01 20:53:57 -0600

    Servicios de cuenta

---
+++ config/routes/account.route.ts
@@ -0,0 +1,18 @@
+import {Router} from 'express';
+import {AccountController} from "../../app/controllers/account.controller";
+
+
+export const ACCOUNT_ROUTER:Router = Router();
+// define routes
+//update
+ACCOUNT_ROUTER.post('/', AccountController.create);
+//getById
+ACCOUNT_ROUTER.get('/:id', AccountController.getById);
+//getAll
+ACCOUNT_ROUTER.post('/getAll', AccountController.getAll);
+//update
+ACCOUNT_ROUTER.put('/:id', AccountController.updateById);
+//delete
+ACCOUNT_ROUTER.delete('/:id', AccountController.deleteById);
+//getMaxAccountId
+ACCOUNT_ROUTER.post('/getMaxAccountId', AccountController.getMaxAccountId);
