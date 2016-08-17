9e8ad495542ce00b57552a22094307d63f9b319a
dfonseca <tuma05@gmail.com>
2016-08-17 12:11:59 -0600

    Incluir store de sessión con MongoDb

--- config/session.ts
+++ config/session.ts
@@ -1,6 +1,12 @@
+import * as session from "express-session";
+const MongoStore = require('connect-mongo')(session);
+
 module.exports.session = {
-	resave: true,
-	name: "connect.tpsid",
-	saveUninitialized: true,
-	secret: 'f134ec88b47384b00060e72c06cd2012'
+  resave: true,
+  name: "connect.tpsid",
+  saveUninitialized: true,
+  secret: 'f134ec88b47384b00060e72c06cd2012',
+  store: new MongoStore({
+    url: 'mongodb://172.16.122.12/test-app'
+  })
 };
