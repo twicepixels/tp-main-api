4ddc79639ea70627b524c32ade1f7cc66470b2bb
Juanjo <ajuanjo@gmail.com>
2016-08-24 19:23:35 -0600

    identacion

--- app/controllers/billing/plan.controller.ts
+++ app/controllers/billing/plan.controller.ts
@@ -2,8 +2,8 @@
  * Created by Juanjo on 19/08/2016.
  */
 
-import { Request, Response } from 'express';
-import { PlanService } from "../../services/billing/plan.service";
+import {Request, Response} from 'express';
+import {PlanService} from "../../services/billing/plan.service";
 
 let c = require("../../../base/base.controller");
 
@@ -11,18 +11,18 @@ export class PlanController {
 
   //amount: 5000, interval: "month", name: "Emerald essentials", currency: "usd", id: "emerald-essentials"
 
-  public static create(req: Request, res: Response) {
-   let _service = new PlanService(req, res);
-   let amount: number = req.body["amount"];
-   let interval: string = req.body["interval"];
-    let name: string = req.body["name"];
-    let currency: string = req.body["currency"];
-    let id: string = req.body["id"];
+  public static create(req:Request, res:Response) {
+    let _service = new PlanService(req, res);
+    let amount:number = req.body["amount"];
+    let interval:string = req.body["interval"];
+    let name:string = req.body["name"];
+    let currency:string = req.body["currency"];
+    let id:string = req.body["id"];
 
-   c.handleService(res, _service.addPlan( amount, interval, name, currency, id ));
-   }
+    c.handleService(res, _service.addPlan(amount, interval, name, currency, id));
+  }
 
-  public static getPlans(req: Request, res: Response) {
+  public static getPlans(req:Request, res:Response) {
     let _service = new PlanService(req, res);
     c.handleService(res, _service.getPlans());
   }
--- app/services/billing/plan.service.ts
+++ app/services/billing/plan.service.ts
@@ -2,42 +2,42 @@
  * Created by Juanjo on 19/08/2016.
  */
 
-import { BillingService } from "./billing.service";
+import {BillingService} from "./billing.service";
 
 export class PlanService extends BillingService {
 
 
-  public addPlan(amount: number, interval: string, name: string, currency: string, id: string): Promise<any> {
+  public addPlan(amount:number, interval:string, name:string, currency:string, id:string):Promise<any> {
 
     //amount: 5000, interval: "month", name: "Emerald essentials", currency: "usd", id: "emerald-essentials"
 
-   let _service = this;
-   return new Promise((resolve: any, reject: any)=> {
-   // _service.getBillingId(accountId, tokenId).then(
-   //   (billingId: string)=> {
-   _service.billingAPI.plans.create({
-       amount: amount,
-       interval: interval,
-       name: name,
-       currency: currency,
-       id: id
-   }).then(
-   (result: any)=>resolve(result),
-   (error: any)=>reject(error)
-   );
-   //   }, (error: any)=>reject(error)
-   // );
-   });
-   }
-
-  public getPlans(): Promise<any> {
     let _service = this;
-    return new Promise((resolve: any, reject: any)=> {
+    return new Promise((resolve:any, reject:any)=> {
+      // _service.getBillingId(accountId, tokenId).then(
+      //   (billingId: string)=> {
+      _service.billingAPI.plans.create({
+        amount: amount,
+        interval: interval,
+        name: name,
+        currency: currency,
+        id: id
+      }).then(
+        (result:any)=>resolve(result),
+        (error:any)=>reject(error)
+      );
+      //   }, (error: any)=>reject(error)
+      // );
+    });
+  }
+
+  public getPlans():Promise<any> {
+    let _service = this;
+    return new Promise((resolve:any, reject:any)=> {
       //_service.getBillingId(accountId).then(
       //(billingId: string)=> {
       _service.billingAPI.plans.list().then(
-        (result: any)=>resolve(result),
-        (error: any)=>reject(error)
+        (result:any)=>resolve(result),
+        (error:any)=>reject(error)
       );
       //}, (error: any)=>reject(error)
       //);
