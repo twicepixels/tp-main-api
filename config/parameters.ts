9af1a55a5289facba7a96890cc138901b49c9996
eduray <egcabc@gmail.com>
2016-09-18 08:56:23 -0600

    Se elimina la clave por seguridad del S3

--- config/parameters.ts
+++ config/parameters.ts
@@ -1,9 +1,9 @@
 module.exports = {
   billingKey: "sk_test_3nWm0n6fDN33ZRxSfOgac8LD",
-  awsAccessKeyId: "AKIAJFPXGOGJVG2N65NQ",
-  awsSecretAccessKey: "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",
+  awsAccessKeyId: "#################",
+  awsSecretAccessKey: "######################",
   awsRegion: "us-west-2",
   awsBucket: "bck-twicepx-data",
-  filesPath: "/Users/eduray/WebstormProjects/core/tp-main-api/",
+  filesPath: "[ruta]",
   folderName: "uploads"
 };
